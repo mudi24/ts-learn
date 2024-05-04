8889521c524b7e5f5d514a9a754e4fd132fea680
yaokai <yaokai@dehuinet.com>
2024-05-04 22:37:35 +0800

    tsconfig.json 配置项

--- index.js
+++ index.js
@@ -10,3 +10,22 @@ console.log(str);
 console.log(num);
 console.log(b1);
 console.log(v2);
+let obj = { name: '', handle: () => { } };
+let fn = (type) => {
+    console.log(type.run);
+};
+let div = document.querySelectorAll('div, footer');
+const axios = {
+    get(url) {
+        return new Promise((resolve, reject) => {
+            let xhr = new XMLHttpRequest();
+            xhr.open('GET', url);
+            xhr.onreadystatechange = () => {
+                if (xhr.readyState === 4 && xhr.status == 200) {
+                    resolve(JSON.parse(xhr.responseText));
+                }
+            };
+            xhr.send(null);
+        });
+    }
+};
